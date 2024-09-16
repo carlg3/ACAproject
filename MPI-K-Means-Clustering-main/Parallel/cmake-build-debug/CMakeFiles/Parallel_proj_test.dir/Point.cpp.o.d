6bcb9426a63dfb04b6d5bccfe91255e5aab59f25
carlg <galante.carl@gmail.com>
2024-09-16 18:01:32 +0200

    x

--- MPI-K-Means-Clustering-main/Parallel/cmake-build-debug/CMakeFiles/Parallel_proj_test.dir/Point.cpp.o.d
+++ MPI-K-Means-Clustering-main/Parallel/cmake-build-debug/CMakeFiles/Parallel_proj_test.dir/Point.cpp.o.d
@@ -157,10 +157,6 @@ CMakeFiles/Parallel_proj_test.dir/Point.cpp.o: \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
  /usr/include/c++/11/bits/istream.tcc \
  /mnt/c/Users/galan/Documents/GitHub/ACAproject/MPI-K-Means-Clustering-main/Parallel/Point.h \
- /usr/include/c++/11/list /usr/include/c++/11/bits/stl_list.h \
- /usr/include/c++/11/bits/allocated_ptr.h \
- /usr/include/c++/11/ext/aligned_buffer.h \
- /usr/include/c++/11/bits/list.tcc \
  /mnt/c/Users/galan/Documents/GitHub/ACAproject/MPI-K-Means-Clustering-main/Parallel/Tupla.h \
  /usr/include/c++/11/vector /usr/include/c++/11/bits/stl_uninitialized.h \
  /usr/include/c++/11/bits/stl_vector.h \
