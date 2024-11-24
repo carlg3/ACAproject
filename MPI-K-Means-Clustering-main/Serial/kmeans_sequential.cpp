7db0cd78f88a19f450e06cede1a90b66fb11d6f0
carlg3 <galante.carl@gmail.com>
2024-11-24 10:29:17 +0000

    aggiunto codice test

---
+++ MPI-K-Means-Clustering-main/Serial/kmeans_sequential.cpp
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <list>
+
+#include "Tupla.h"
+#include "Cluster.h"
+#include "Point.h"
+
+#include <chrono>
+using namespace std;
+using namespace chrono;
+
+int K = 0; // Number of Clusters
+int MAXITERATION = 5;
+
+void readDataSet(int *pointDimension,int *totalNumberPoint){
+	string buffer;
+	ifstream DataSet; DataSet.open("/home/galan/ACAproject/MPI-K-Means-Clustering-main/DataSet/DataSet10000x10.txt");
+	if(!DataSet.is_open()){
+		cout << "FILE OPENING FAILED" << endl;
+		return;
+	}
+	while (getline(DataSet, buffer)){
+		vector <string> tokens;
+		stringstream check1(buffer);
+		string intermediate;
+		while(getline(check1, intermediate, ',')){
+			tokens.push_back(intermediate);
+		}
+		Point *point = new Point(tokens.size());
+		for(int i = 0; i < (int)tokens.size(); i++){
+			point->setThValue(i,stod(tokens[i]));
+		}
+		*pointDimension = tokens.size();
+		*totalNumberPoint = *totalNumberPoint + 1;
+	}
+	DataSet.close();
+}
+
+int main(){
+	//time_t start, end;
+	//time(&start);
+	
+	auto start = high_resolution_clock::now(); // Inizia il timer
+
+	int pointDimension;    // Dimensione del dato R^pointDimension
+	int totalNumberPoint = 0; // Numbers of data in our DataSet
+
+	//srand(time(0)); // Randomize initialization point
+
+	// READING FILE
+	readDataSet(&pointDimension,&totalNumberPoint);
+
+	// INIZIALIZE CLUSTERS AND CENTROIDS
+	K = sqrt(totalNumberPoint/2);
+	Cluster::createKclusters(K,pointDimension);
+	// INITIAL TMSE
+	double previousTMSE = 0;
+	double tmse = 0;
+	//Cluster::printClusters();
+
+	while((MAXITERATION-- && tmse<previousTMSE) || previousTMSE==0) {
+		Cluster::clustersReset();
+		// ASSIGN POINTS TO CLUSTERS
+		Cluster::pointAssignment();
+		// CALCULE CENTROIDS
+		Cluster::centroidsAssignment();
+		previousTMSE = tmse;
+		tmse = Cluster::totalMSE();
+	}
+
+	// time(&end);
+	// double time_taken = double(end - start);
+	// cout << "Time taken by program is : " << time_taken << " sec " << endl;
+	// Usando chrono...
+	auto end = high_resolution_clock::now(); // Termina il timer
+	auto duration = duration_cast<microseconds>(end - start).count(); // Tempo in microsecondi
+	
+	cout << "Time taken to run the task: " << duration << " sec" << endl;
+	return 0;
+}
