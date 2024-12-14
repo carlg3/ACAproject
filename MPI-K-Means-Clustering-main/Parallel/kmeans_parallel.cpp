ac23a6dcebcdcd9dbbd025b61d4f46909d2fca40
Carlo <galante.carl@gmail.com>
2024-12-14 19:33:20 +0100

    Update

--- MPI-K-Means-Clustering-main/Parallel/kmeans_parallel.cpp
+++ MPI-K-Means-Clustering-main/Parallel/kmeans_parallel.cpp
@@ -12,9 +12,7 @@ using namespace std;
 int MAXITERATION = 10;
 const int LENTAG = 0, STAT = 1, DATAPOINTTAG = 2, DATACLUSTERTAG = 3, DATASUMCLUSTERTAG = 4;
 
-const char* path_win = "/mnt/c/Users/galan/Documents/GitHub/ACAproject/MPI-K-Means-Clustering-main/DataSet/DataSet50000x10.txt";
 // const char* path_gcloud = "/home/galan/ACAproject/MPI-K-Means-Clustering-main/DataSet/DataSet50000x10.txt";
-
 const char* path_gcloud = "/mnt/c/Users/galan/Documents/GitHub/ACAproject/MPI-K-Means-Clustering-main/DataSet/DataSet50000x10.txt";
 
 void writeExTime(int cs, int tnp, int pd, int K, double time){
